9efbb5dc0b3f6bc8de87be592fe71a52dc66c906
plmsmile <plmspark@163.com>
2018-01-05 22:41:00 +0800

    del_dups_sortedlist solve2

--- 18_del_node_in_list/del_dups_sortedlist.cpp
+++ 18_del_node_in_list/del_dups_sortedlist.cpp
@@ -80,18 +80,8 @@ ListNode* del_dups_nosave(ListNode* head) {
     ListNode* pre = nullptr;
     ListNode* next = nullptr;
     while (cur) {
-        // 0. 最后一个元素，后面已经没有值
-        if (cur->next == nullptr) {
-            if (pre == nullptr) {
-                pre = cur;
-                head = pre;
-            } else {
-                pre->next = cur;
-            }
-            break;
-        }
         // 1. cur无重复
-        else if (cur->val != cur->next->val) {
+        if (cur->next == nullptr || cur->val != cur->next->val) {
             if (pre == nullptr) {
                 pre = cur;
                 head = pre;
@@ -113,6 +103,7 @@ ListNode* del_dups_nosave(ListNode* head) {
             // 2.2 删除cur
             delete cur;
             cur = next;
+            // 原本pre->next=cur，但是当前cur已经被删除，所以重新置为空
             if (pre != nullptr)
                 pre->next = nullptr;
         }
@@ -124,19 +115,71 @@ ListNode* del_dups_nosave(ListNode* head) {
 }
 
 
+
+ListNode* del_dups_nosave2(ListNode* head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+    ListNode* pre = nullptr;
+    ListNode* cur = head;
+    while (cur != nullptr) {
+        // 1. 先计算cur是否需要删除
+        bool need_delete = false;
+        ListNode* next = cur->next;
+        if (next != nullptr && cur->val == next->val) {
+            need_delete = true;
+        }
+        // 2. cur不重复，无需删除
+        if (need_delete == false) {
+            if (pre == nullptr) {
+                pre = cur;
+                head = pre;
+            } else {
+                // 当前续接到pre后面，pre后移
+                pre->next = cur;
+                pre = pre->next;
+            }
+            cur = next;
+        } 
+        // 3. cur重复，需要被删除
+        else {
+            int value = cur->val;
+            ListNode* to_del = cur;
+            // 删掉所有是value的节点
+            while (to_del != nullptr && to_del->val == value) {
+                next = to_del->next;
+                delete to_del;
+                to_del = nullptr;
+                to_del = next;
+            }
+            cur = to_del;
+            // 本来pre.next=cur，结果cur给删除了，所以手动pre.next=nullptr
+            if (pre != nullptr) {
+                pre->next = nullptr;
+            }
+        }
+    }
+    if (pre == nullptr) {
+        head = nullptr;
+    }
+    return head;
+
+}
+
 /*
  * 测试无保留删除
  */
 void test_del_nosave() {
-    ListNode* p1 = new ListNode(2);
+    ListNode* p1 = new ListNode(1);
     ListNode* p2 = new ListNode(2);
-    ListNode* p3 = new ListNode(2);
-    ListNode* p4 = new ListNode(2);
+    ListNode* p3 = new ListNode(3);
+    ListNode* p4 = new ListNode(4);
     p1->next = p2;
     p2->next = p3;
     p3->next = p4;
     show_list(p1);
-    p1 = del_dups_nosave(p1);
+    // p1 = del_dups_nosave(p1);
+    p1 = del_dups_nosave2(p1);
     show_list(p1);
 }
 
