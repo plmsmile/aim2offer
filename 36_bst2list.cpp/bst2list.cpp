8cbe192f614765019e6bed056506621af7de2dd7
plmsmile <plmspark@163.com>
2018-01-24 16:51:14 +0800

    bst2list

--- 36_bst2list.cpp/bst2list.cpp
+++ 36_bst2list.cpp/bst2list.cpp
@@ -6,6 +6,10 @@
  * @date 2018-01-21
  */
 
+#include <stack>
+
+using namespace std;
+
 
 struct TreeNode {
     int val;
@@ -45,7 +49,7 @@ void convert_inorder(TreeNode* root, TreeNode* &pre) {
 
 
 /*
- * 把树转化为链表
+ * 把树转化为链表，递归版本
  * Args:
  *      root -- 树
  * Returns:
@@ -63,6 +67,48 @@ TreeNode* convert(TreeNode* root) {
         pre = pre->left;
     }
     return pre;
+}
+
+
+/*
+ * 非递归中序遍历树，转换为双向链表
+ */
+TreeNode* convert_stack(TreeNode* root) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    stack<TreeNode*> st;
+    TreeNode* p = root;
+    // 上一个节点
+    TreeNode* pre = nullptr;
+    while (p || !st.empty()) {
+        if (p) {
+            // p入栈，接着向左走
+            st.push(p);
+            p = p->left;
+        } else {
+            // p为空，出栈元素，p为根节点，左孩子已经访问结束或者没有左孩子
+            p = st.top();
+            st.pop();
+            if (pre == nullptr) {
+                pre = p;
+            } else {
+                pre->right = p;
+                p->left = pre;
+                pre = p;
+            }
+            // 扫描右孩子
+            p = p->right;
+        }
+    }
+
+    // 找到头结点
+    while (pre && pre->left) {
+        pre = pre->left;
+    }
+    return pre;
+
 }
 
 
